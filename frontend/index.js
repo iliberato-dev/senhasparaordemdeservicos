d9fd0d345816c16f0d2ce969220e24c6d14a4414
iliberato dev <118570733+iliberato-dev@users.noreply.github.com>
2025-05-30 11:41:00 -0300

    Update index.js

--- frontend/index.js
+++ frontend/index.js
@@ -38,41 +38,13 @@ async function gerarSenhasDeterministicas() {
     }
 }
 
-// Adicione um botão para buscar as senhas (opcional)
-// async function buscarSenhas() {
-//     const ordemServicoInput = document.getElementById('ordemServico');
-//     const senha4DigitosElement = document.getElementById('senha4Digitos');
-//     const senha6DigitosElement = document.getElementById('senha6Digitos');
-//     const ordemServico = ordemServicoInput.value;
-
-//     if (ordemServico) {
-//         try {
-//             const response = await fetch(`http://localhost:3001/buscar-senhas/${ordemServico}`);
-
-//             if (response.status === 404) {
-//                 senha4DigitosElement.textContent = 'Nenhuma senha encontrada para esta ordem de serviço.';
-//                 senha6DigitosElement.textContent = '';
-//                 return;
-//             }
-
-//             if (!response.ok) {
-//                 const error = await response.json();
-//                 senha4DigitosElement.textContent = `Erro ao buscar: ${error.error || 'Falha ao buscar as senhas.'}`;
-//                 senha6DigitosElement.textContent = '';
-//                 return;
-//             }
-
-//             const data = await response.json();
-//             senha4DigitosElement.textContent = `Senha de 4 dígitos: ${data.senha4}`;
-//             senha6DigitosElement.textContent = `Senha de 6 dígitos: ${data.senha6}`;
-
-//         } catch (error) {
-//             console.error("Erro ao comunicar com o servidor:", error);
-//             senha4DigitosElement.textContent = 'Erro ao comunicar com o servidor.';
-//             senha6DigitosElement.textContent = '';
-//         }
-//     } else {
-//         senha4DigitosElement.textContent = 'Por favor, insira a ordem de serviço para buscar.';
-//         senha6DigitosElement.textContent = '';
-//     }
-// }
+// Add event listener for the 'keydown' event on the input field
+document
+  .getElementById("ordemServico")
+  .addEventListener("keydown", function (event) {
+    // Check if the pressed key is 'Enter'
+    if (event.key === "Enter") {
+      event.preventDefault(); // Prevent default form submission behavior (e.g., page reload)
+      gerarSenhasDeterministicas(); // Call the password generation function
+    }
+  });
